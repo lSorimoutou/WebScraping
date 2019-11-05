798ed33830df8199fec83536342ff706af82f7b2
lenaick <lenaick.sorimoutou@gmail.com>
2019-11-06 00:36:01 +0100

    add collapse menu in card-produit component

--- client/src/Components/card-produit.components/card-produit.components.jsx
+++ client/src/Components/card-produit.components/card-produit.components.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Collapse from '@material-ui/core/Collapse';
+import CardActions from '@material-ui/core/CardActions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
+
 
 const useStyles = makeStyles(theme => ({
     card: {
@@ -34,18 +38,19 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+
+
 export default function RecipeReviewCard(props) {
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
 
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
     return (
         <Card className={classes.card}> 
             <CardHeader
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                    </IconButton>
-                }
                 title={props.name}
                 subheader={props.info + " " + props.weight}
             />
@@ -57,10 +62,31 @@ export default function RecipeReviewCard(props) {
             <CardContent>
                 <Typography variant="h5" color="error" component="P" align='center'>
                     {props.price + "€"}
-        </Typography>
+                </Typography>
             </CardContent>
+
             <CardActions disableSpacing>
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
             </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography variant="h6">Description</Typography>
+                    <Typography variant="p" paragraph='true'>{props.desc}</Typography>
+                    <Typography variant="h6">Ingrédients</Typography>
+                    <Typography variant="p" paragraph='true'>{props.ingredients}</Typography>
+                    <Typography variant="h6">INFORMATIONS NUTRITIONNELLES</Typography>
+                    <Typography variant="p" paragraph='true'>{props.infoNutri}</Typography>
+                </CardContent>
+            </Collapse>
         </Card>
     );
 }
